1975bb900233a4625159cd986f5e695be92ddfc2
Matteo Barbero <66015486+maiobarbero@users.noreply.github.com>
2023-06-05 15:31:56 +0200

    First commit

---
+++ Shell_Abstract_Boilerplate.php
@@ -0,0 +1,151 @@
+<?php
+
+/*
+ * Magento 1.9 Shell Abstract Boilerplate
+ * Author: Matteo Barbero
+ */
+
+//Change your path
+require_once __DIR__ . '/../abstract.php';
+
+class Shell_Abstract_Boilerplate extends Mage_Shell_Abstract
+{
+  const COLOR_RED = "\033[31m";
+  const COLOR_GREEN = "\033[32m";
+  const COLOR_YELLOW = "\033[33m";
+  const COLOR_RESET = "\033[0m";
+
+  private $startTime;
+
+  /**
+   * Constructor. Initializes the start time.
+   */
+  public function __construct()
+  {
+    parent::__construct();
+    $this->startTime = microtime(true);
+  }
+
+  /**
+   * Entry point of the shell script.
+   */
+  public function run()
+  {
+    // Run your methods here
+
+    $this->endTimer();
+  }
+
+  /**
+   * Displays the usage help text.
+   *
+   * @return string
+   */
+  public function usageHelp()
+  {
+    return <<<USAGE
+\n
+Write here your help text...
+\n
+USAGE;
+  }
+
+  /**
+   * Retrieve the current store ID.
+   *
+   * @return int
+   */
+  protected function getCurrentStoreId()
+  {
+    return Mage::app()->getStore()->getStoreId();
+  }
+
+  /**
+   * Retrieve the current store code.
+   *
+   * @return string
+   */
+  protected function getCurrentStoreCode()
+  {
+    return Mage::app()->getStore()->getCode();
+  }
+
+  /**
+   * Retrieve the current store name.
+   *
+   * @return string
+   */
+  protected function getCurrentStoreName()
+  {
+    return Mage::app()->getStore()->getName();
+  }
+
+  /**
+   * Retrieve the base URL for the current store.
+   *
+   * @return string
+   */
+  protected function getBaseUrl()
+  {
+    return Mage::getBaseUrl();
+  }
+
+  /**
+   * Retrieve the base URL for the current store with a specified path.
+   *
+   * @param string $path
+   * @return string
+   */
+  protected function getUrl($path = '')
+  {
+    return Mage::getUrl($path);
+  }
+
+  /**
+   * Retrieve the value of a configuration setting.
+   *
+   * @param string $path
+   * @param int $storeId
+   * @return mixed
+   */
+  protected function getConfigValue($path, $storeId = null)
+  {
+    return Mage::getStoreConfig($path, $storeId);
+  }
+  /**
+   * Ends the timer and displays the script execution time.
+   */
+  public function endTimer()
+  {
+    $endTime = microtime(true);
+    $executionTime = $endTime - $this->startTime;
+    echo "\n\nScript execution time: " . round($executionTime, 2) . " seconds\n\n";
+  }
+
+  /**
+   * Prints colored output based on the provided color.
+   *
+   * @param string $string The string to be printed
+   * @param string $color The color to be applied (red, yellow, green)
+   */
+  public function colorOutput($string, $color)
+  {
+    switch ($color) {
+      case 'red':
+        echo self::COLOR_RED . $string . self::COLOR_RESET;
+        break;
+      case 'yellow':
+        echo self::COLOR_YELLOW . $string . self::COLOR_RESET;
+        break;
+      case 'green':
+        echo self::COLOR_GREEN . $string . self::COLOR_RESET;
+        break;
+      default:
+        echo $string;
+        break;
+    }
+  }
+}
+
+$shell = new Shell_Abstract_Boilerplate();
+$shell->run();
---
+++ readme.md
@@ -0,0 +1,33 @@
+# Magento 1.9 Shell Abstract Boilerplate
+
+This is a boilerplate for creating shell scripts in Magento 1.9. It provides a foundation for building custom shell scripts to perform various tasks within a Magento 1.9 installation.
+
+## Features
+
+- Abstract class that extends `Mage_Shell_Abstract`
+- Basic structure with essential methods
+- Helper methods for common tasks
+- Customizable and extensible for specific use cases
+
+## Prerequisites
+
+- Magento 1.9.x installation
+
+## Usage
+
+1. Copy the `Boilerplate_Shell.php` file to your Magento 1.9 installation's shell directory.
+2. Customize the boilerplate to add your own functionality and methods.
+3. Run your custom shell script by executing the `php Boilerplate_Shell.php` command from the shell directory.
+
+## Customization
+
+You can extend the `Boilerplate_Shell` class and add your own methods and functionality to tailor it to your specific use case. Feel free to modify the provided methods or add new ones as needed.
+
+## License
+
+This Magento 1.9 Shell Abstract Boilerplate is released under the [MIT License](LICENSE).
+
+## Author
+
+- Created by Matteo Barbero
+- Linkedin: [@MatteoBarbero](https://www.linkedin.com/in/matteo-barbero-b04647155/)
